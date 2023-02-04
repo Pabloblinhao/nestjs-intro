c4e49a2dc5c8f2d87047c6a77337a45eabbded35
pablo <pablo_vitor1707@hotmail.com>
2023-02-04 20:01:07 -0300

    feat(table): create `Service`

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post } from "@nestjs/common";
+import { create } from "domain";
+import { TableService } from "./table.service";
 
 @Controller('table')
 export class TableController {
+    constructor(private tableService: TableService) {}
+
      @Get()
      findAll() {
-        return 'Buscar todas as mesas';
-    }
+        return this.tableService.findAll();
+     }
+     @Post()
+    create() {
+        return this.tableService.create();
+    } 
+    
 
 }
+
+
---
+++ src/table/table.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+
+@Injectable()
+export class TableService {
+
+ 
+ findAll() {
+        return 'Buscar todas as mesas';
+    }
+
+    create() {
+        return 'Criar uma mesa';
+    }
+}
